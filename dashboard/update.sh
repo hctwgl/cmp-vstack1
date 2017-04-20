48f48ff071e7797623ad63764c37673c4b3fa03b
netgenius <wuheng09@otcaix.iscas.ac.cn>
2017-04-20 09:24:56 +0800

    add dashboard

---
+++ dashboard/update.sh
@@ -0,0 +1,6 @@
+python setup.py sdist
+cp dist/zstack_dashboard-*.tar.gz /usr/local/zstack/apache-tomcat-7.0.35/webapps/zstack/WEB-INF/classes/tools/
+/etc/init.d/zstack-dashboard stop  
+zstack-ctl install_ui
+/etc/init.d/zstack-dashboard start
+
